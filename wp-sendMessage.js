7507c6574c8a80b3fd23954aae28556d74fa6d12
Alonso Otero <alonsooteroseminario@gmail.com>
2020-10-22 16:34:33 -0300

    16:34 update

--- wp-sendMessage.js
+++ wp-sendMessage.js
@@ -5,14 +5,17 @@ const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
+function send_whatsApp() {
 
   client.messages.create({
     from: 'whatsapp:+14155238886',
     //id=chat-input
     body: 'Hi bimmer! I´m webclash.io, your web tool for clash detection. \nYou have to check the next link to view your clash review. Check it out here: https://webclash-viewer.herokuapp.com/ ',
-    //
     to: 'whatsapp:+56956942823',
-    // mediaUrl: './images/andy.png'
+    mediaUrl: ['https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'],
   }).then(message => console.log(message.sid));
 
+}
+
+send_whatsApp();
 
