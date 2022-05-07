e1f8d0993cf3af070184da38f73616102681113f
Luis Otero <alonsooteroseminario@gmail.com>
2022-05-06 20:44:01 -0700

    works ok

--- start.js
+++ start.js
@@ -12,7 +12,16 @@ if (config.credentials.client_id == null || config.credentials.client_secret == 
 
 let app = express();
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')));
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('/dist'))
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname,  'dist', 'index.html'))
+    })
+}
+else{
+    app.use(express.static(path.join(__dirname, 'public')));
+}
+
 app.use(express.json({ limit: '800mb' }));
 app.use('/api/forge/oauth', require('./routes/oauth'));
 app.use('/api/forge/oss', require('./routes/oss'));
@@ -22,4 +31,7 @@ app.use((err, req, res, next) => {
     console.error(err);
     res.status(err.statusCode).json(err);
 });
+
+
+
 app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
